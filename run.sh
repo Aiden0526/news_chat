82cd3c33351f6e385413f449e94af6a0276401c1
Aiden Xu <aidenxu@MacBook-Pro-2.local>
2023-06-20 01:21:04 +0800

    commit

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-API_key="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"  ## please replace with your openai api
+API_key="you api key"  ## please replace with your openai api
 QUESTION="What could be the risk of AI?" ## please replace with your question
 
 python main.py --api_key $API_key --question "$QUESTION"
